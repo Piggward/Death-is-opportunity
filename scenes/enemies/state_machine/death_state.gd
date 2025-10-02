fb5fbbb0c2c9d87ab73ebd974a72374b851ace85
= <=>
2025-10-02 21:28:33 +0200

    almost done with core states

--- scenes/enemies/state_machine/death_state.gd
+++ scenes/enemies/state_machine/death_state.gd
@@ -4,4 +4,5 @@ const RESURRECTABLE_AREA = preload("uid://4upasttw8fh8")
 func enter() -> void:
 	enemy.animated_sprite.die()
 	var area: RessurectableArea = RESURRECTABLE_AREA.instantiate()
+	enemy.character.attack_area.monitoring = false
 	area.character = enemy.character
